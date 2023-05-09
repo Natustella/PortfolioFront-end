cee017807fcbc9157d4af514e3ef51de67f7fe11
Natustella <natustella@gmail.com>
2023-05-09 12:11:36 -0300

    environment

--- src/environments/environment.prd.ts
+++ src/environments/environment.prd.ts
@@ -1,3 +1,9 @@
 export const environment = {
     apiURL: "https://portfolio-back-end-o1rr.onrender.com/",
+    idpersona: 1,
+    skills: [],
+    curso: [],
+    experiencia: [],
+    proyecto: [],
+    redes: []
 };
